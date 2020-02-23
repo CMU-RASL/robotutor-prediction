8238da94c4cbe20ad9ced84065ba408c96fc2798
Steven Qu <stevenqu2001@gmail.com>
2020-02-22 22:32:54 -0500

    implemented naive feedback screen classifier

--- direct_VF.py
+++ direct_VF.py
@@ -1,21 +1,59 @@
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+
+def feedBack(yellow,green,red):
+    yc, ycount = np.unique(yellow.reshape(-1,yellow.shape[-1]),
+                          axis=0, return_counts=True)
+    gc, gcount = np.unique(green.reshape(-1,green.shape[-1]),
+                          axis=0, return_counts=True)
+    rc, rcount = np.unique(red.reshape(-1,red.shape[-1]),
+                          axis=0, return_counts=True)
+    if np.array_equal(yc[ycount.argmax()], [50,236,250]) and np.array_equal(gc[gcount.argmax()], [68,228,55]) and np.array_equal(rc[rcount.argmax()], [55,54,232]):
+        return True
+    return False
+
+def feedBackType(frame):
+    height, width, channels = frame.shape
+    yellow = frame[height // 2 - 110 : height // 2 - 100, width // 4 - 25 : width // 4 + 25]
+    green = frame[height // 5 + 65 : height // 5 + 75, width // 4 - 25 : width // 4 + 25]
+    red = frame[4*height // 5 - 85 : 4*height // 5 - 75, width // 4 - 25 : width // 4 + 25]
+
+    yc, ycount = np.unique(yellow.reshape(-1,yellow.shape[-1]), axis=0, return_counts=True)
+    gc, gcount = np.unique(green.reshape(-1,green.shape[-1]), axis=0, return_counts=True)
+    rc, rcount = np.unique(red.reshape(-1,red.shape[-1]), axis=0, return_counts=True)
+
+    g= gc[gcount.argmax()]
+    y = yc[ycount.argmax()]
+    r = rc[rcount.argmax()]
+
+    if (np.array_equal(g, [208, 246, 208])):
+        return "green"
+    elif (np.array_equal(y, [207, 244, 248])):
+        return "yellow"
+    elif (np.array_equal(r, [207, 208, 247])):
+        return "red"
+    return None
+
 
 def gray_screen(video_filename):
 
-    cap = cv2.VideoCapture('data/video/'+video_filename)
+    cap = cv2.VideoCapture('data/video/05.mp4') #video name
+    prevFrame = None
 
     sides = []
     #is this the length of the video?
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    print( length )
     seenGray = False
     fps = cap.get(cv2.CAP_PROP_FPS)
     start = []
     end = []
-    num = 0
+    num = 1
+
+    feedBack_num = 1
+    seenfeedBack = False
 
     while(cap.isOpened()):
 
@@ -29,15 +67,35 @@ def gray_screen(video_filename):
             break
 
         height, width, channels = frame.shape
+        yellow = frame[height // 2 - 10 : height // 2 + 10, width // 4 - 25 : width // 4 + 25]
+        green = frame[height // 5 - 10 : height // 5 + 10, width // 4 - 25 : width // 4 + 25]
+        red = frame[4*height // 5 - 10 : 4*height // 5 + 10, width // 4 - 25 : width // 4 + 25]
+
         snap = frame[height//2 - 50 : height//2 + 50,
                           width // 2 - 100 : width // 2 + 100,:]
 
+        '''
+        cv2.imshow("pic", snap)
+        cv2.imshow("frame", frame)
+        cv2.waitKey(2)
+        '''
+
+        if feedBack(yellow, green, red):
+            seenfeedBack = True
+        else:
+            if seenfeedBack and not np.array_equal(prevFrame, None):
+                seenfeedBack = False
+                cv2.imwrite('tempData/activity_time/frame'+str(feedBack_num)+'.jpg', prevFrame)
+                #print(cap.get(cv2.CAP_PROP_POS_MSEC))
+                feedBack_num += 1
+                f = feedBackType(prevFrame) #change this variable name
+                if (f == None):
+                    print("could not classify")
+                else:
+                    print(f)
+        '''
         colors, count = np.unique(snap.reshape(-1,snap.shape[-1]),
                                   axis=0, return_counts=True)
-
-        if [244,249,255] in colors and len(colors) == 1:
-            print(colors)
-            cv2.imwrite('data/activity_time/feedbackFrame.jpg', frame)
 
         if not seenGray and len(colors) == 1 and np.array_equal(colors[0], [136,136,136]) :
             seenGray = True
@@ -49,14 +107,16 @@ def gray_screen(video_filename):
         elif seenGray == True and len(colors) != 1:
             seenGray = False
             end.append(cap.get(cv2.CAP_PROP_POS_FRAMES)/fps)
-            cv2.imwrite('data/activity_time/frame' + str(num) +'.jpg', frame)
+            cv2.imwrite('tempData/activity_time/frame' + str(num) +'.jpg', frame)
             print("end", cap.get(cv2.CAP_PROP_POS_MSEC))
             num += 1
     # When everything done, release the capture
-
+        '''
+        prevFrame = frame
     cap.release()
     cv2.destroyAllWindows()
-    f = open('data/activity_time/' +
+    '''
+    f = open('tempData/activity_time/' +
          video_filename[:2]+'activity.txt', 'w+')
     for i in range(max(len(start), len(end))):
         if i < len(start):
@@ -64,9 +124,8 @@ def gray_screen(video_filename):
         if i < len(end):
             f.write("end: " + str(end[i]) + "\n")
     f.close()
-
+    '''
 def main():
-    print ("start")
     video_filenames = os.listdir('data/video')
     gray_screen("01.mp4")
 
